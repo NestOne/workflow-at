4579eedd2a189bb4fb00862d7ef8272f6707313f
samadammeek <sam.meek@gmail.com>
2015-01-14 11:47:08 +0000

    commit 2

---
+++ jbpm-WPS-client/src/main/java/cobweb/m24/NewProcessTest.java
@@ -0,0 +1,36 @@
+package cobweb.m24;
+
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.process.WorkItemHandler;
+
+public class NewProcessTest {
+	
+	
+	public static void main (String args[]){
+	
+	KieServices ks = KieServices.Factory.get();
+	KieContainer kContainer = ks.getKieClasspathContainer();
+	//KieBase kbase = kContainer.getKieBase("kbase");
+	KieSession ksession = kContainer.newKieSession("ksession-process");
+
+/**	RuntimeManager manager = createRuntimeManager(kbase);
+	RuntimeEngine engine = manager.getRuntimeEngine(null);
+	KieSession ksession = engine.getKieSession();
+	TaskService taskService = engine.getTaskService();**/
+	ksession.getWorkItemManager().registerWorkItemHandler("Pillar 4 - Point In Polygon", 
+			(WorkItemHandler) new GenericWorkItemHandlerClient());
+	
+	String wpsURL = "http://localhost:8010/wps/WebProcessingService?";
+	String processDescription = "pillar.authoritativedata.PointInPolygon";
+	
+	String inputObservations = "https://dyfi.cobwebproject.eu/geoserver/FloodingData/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=FloodingData:observations&outputFormat=text/xml;%20subtype=gml/3.1.1";
+	String inputAuthoritativeData = "http://grasp.nottingham.ac.uk:8010/geoserver/CobwebTest/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=CobwebTest:Biospheric_Reserves&maxFeatures=50&outputFormat=gml3&srsName=EPSG:4236";
+	
+	ksession.startProcess("cobweb.m24.test_process");
+	
+	}
+
+}
