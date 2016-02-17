e1ccdd61fcd9290431ac6e3090a318826188055f
Julian Rosser <julianrosser@gmail.com>
2016-02-17 10:59:10 +0000

    added starting jkw processes

---
+++ jbpm-WPS-client/src/main/java/cobweb/m24/LosProcessTest.java
@@ -0,0 +1,50 @@
+package cobweb.m24;
+
+import org.jbpm.bpmn2.handler.SignallingTaskHandlerDecorator;
+import org.kie.api.KieBase;
+
+import net.opengis.examples.packet.GMLPacketDocument;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.process.WorkItemHandler;
+import cobweb.m24.ExceptionServiceHandler;
+import org.n52.wps.io.datahandler.parser.GML3BasicParser;
+
+public class LosProcessTest {
+	/**
+	 * @author Julian Rosser
+	 * @param args
+	 * 
+	 *            Main class harness for testing LoS / JKW workflow
+	 */
+
+	public static void main(String args[]) {
+
+		KieServices ks = KieServices.Factory.get();
+		KieContainer kContainer = ks.getKieClasspathContainer();
+		KieBase kbase = kContainer.getKieBase("kbase");
+		KieSession ksession = kContainer.newKieSession("ksession-process");
+
+		String eventType = "error-code";
+		SignallingTaskHandlerDecorator signallingTaskWrapper = new SignallingTaskHandlerDecorator(
+				GenericWorkItemHandlerClient.class, eventType);
+		signallingTaskWrapper
+		.setWorkItemExceptionParameterName(ExceptionServiceHandler.exceptionParameterName);
+
+		
+		ksession.getWorkItemManager().registerWorkItemHandler(
+				"GetLineOfSight", signallingTaskWrapper);
+		
+		
+				
+		/**
+		 * use this to start a defined process, this can be found in
+		 * /src/main/resources
+		 */
+		ksession.startProcess("cobweb.m24.qaqc_knotweed_los");
+
+	}
+
+}
