7ef1129a109d8897ed46524b61b4eb51b5f891db
Julian Rosser <julianrosser@gmail.com>
2016-09-05 15:01:55 +0100

    workflows with suitability/proximity check

--- jbpm-WPS-client/src/main/java/cobweb/m24/qaqc/Local_RProximitySuitabilityPolygonScoreTest.java
+++ jbpm-WPS-client/src/main/java/cobweb/m24/qaqc/Local_RProximitySuitabilityPolygonScoreTest.java
@@ -4,14 +4,19 @@ import cobweb.m24.*;
 
 import org.jbpm.bpmn2.handler.SignallingTaskHandlerDecorator;
 import org.kie.api.KieBase;
+
+import net.opengis.examples.packet.GMLPacketDocument;
+
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.process.WorkItemHandler;
 
 import cobweb.m24.ExceptionServiceHandler;
-import cobweb.m24.GenericWorkItemHandlerClient;
 
-public class Local_RLosLocationQualityLaplaceSuitabilityTest {
+import org.n52.wps.io.datahandler.parser.GML3BasicParser;
+
+public class Local_RProximitySuitabilityPolygonScoreTest {
 
 	public static void main(String args[]) {
 
@@ -25,28 +30,17 @@ public class Local_RLosLocationQualityLaplaceSuitabilityTest {
 				GenericWorkItemHandlerClient.class, eventType);
 		signallingTaskWrapper
 		.setWorkItemExceptionParameterName(ExceptionServiceHandler.exceptionParameterName);
-		
-		ksession.getWorkItemManager().registerWorkItemHandler(
-				"GetLineOfSight", signallingTaskWrapper);
-		
-		ksession.getWorkItemManager().registerWorkItemHandler(
-				"LaplacePhotoBlurCheck", signallingTaskWrapper);
-		
-		ksession.getWorkItemManager().registerWorkItemHandler(
-				"Pillar2LocationQuality",
-				new GenericWorkItemHandlerClient());
+
 		
 		ksession.getWorkItemManager().registerWorkItemHandler(
 				"Pillar5ProximitySuitabilityPolygonScore",
 				new GenericWorkItemHandlerClient());
 		
-		
-		
 		/**
 		 * use this to start a defined process, this can be found in
 		 * /src/main/resources
 		 */
-		ksession.startProcess("cobweb.m24.qaqc.local_qaqc_knotweed_los_locationquality_laplace_suitability");
+		ksession.startProcess("cobweb.m24.qaqc.local_qaqc_knotweed_suitability");
 	}
 
 }
